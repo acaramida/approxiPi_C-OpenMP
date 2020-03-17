f457fe9dbfb1b87fabf19474f14fc8840c1407fc
a.caramida@campus.fct.unl.pt <a.caramida@campus.fct.unl.pt>
2020-03-17 15:50:48 +0000

    OpenMp is kinda broken, will fix later

--- PI.c
+++ PI.c
@@ -1,25 +1,40 @@
-#include  <stdio.h>
-#include <stdlib.h>
-#include  <omp.h>
+#include "omp.h"
+#include "stdio.h"
+#include "stdlib.h"
 
-int main(int argc, char *argv[])
-{
-  double num_steps = atof(argv[1]);
-  double sum = 0.0;
+double points = 0;
+double sum = 0;
 
-  unsigned int seed = omp_get_wtime();
+void* numbers_in(void *arg) {
 
-  for (int i = 0; i < num_steps; i++) {
-      double x = ((double)rand_r(&seed) / RAND_MAX);
+	unsigned int seed = (unsigned int)omp_get_wtime();
+	double p_in = 0;
+	int *threads = (int*)arg;
+
+	for( int i = 0; i < points/ *threads; i++ )
+	{
+			double x = ((double)rand_r(&seed) / RAND_MAX);
       double y = ((double)rand_r(&seed) / RAND_MAX);
 
-      if((1-x)*(1-x) + (1-y)*(1-y) <= 1) {
-        sum++;
-      }
+			if(x * x + y * y <= 1) {
+				p_in++;
+			}
+	}
+
+	sum+=p_in;
+}
+
+void main( int argc, const char* argv[] )
+{
+	int threads = atof(argv[2]);
+	points = atof(argv[1]);
+
+  #pragma omp prallel num_threads(threads){
+    int ID = omp_get_thread_num();
+
+    printf(" hello (%d)\n", ID);
+    printf(" points: %lf \n", points);
+    printf(" points in: %lf \n", sum);
+    printf(" PI estimation: %lf \n", sum/points * 4.0);
   }
-
-  printf(" points: %lf \n", num_steps);
-  printf(" points in: %lf \n", sum);
-  printf(" PI estimation: %lf \n", sum/num_steps * 4.0);
-
 }
