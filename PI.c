c132084b810fa6352cbb5f4073e7d8f2b187aa0f
a.caramida@campus.fct.unl.pt <a.caramida@campus.fct.unl.pt>
2020-03-17 12:35:31 +0000

    sequencial is done, nr points is hard coded will do input next

--- PI.c
+++ PI.c
@@ -1,12 +1,25 @@
 #include  <stdio.h>
+#include <stdlib.h>
 #include  <omp.h>
 
 int main(int argc, char *argv[])
 {
-  #pragma omp parallel
-  {
-    int ID = 0;
-    printf(" hello(%d) ", ID);
-    printf(" world(%d) \n", ID);
+  double num_steps = 1000;
+  double sum = 0.0;
+
+  unsigned int seed = omp_get_wtime();
+
+  for (int i = 0; i < num_steps; i++) {
+      double x = ((double)rand_r(&seed) / RAND_MAX);
+      double y = ((double)rand_r(&seed) / RAND_MAX);
+      
+      if((1-x)*(1-x) + (1-y)*(1-y) <= 1) {
+        sum++;
+      }
   }
+
+  printf(" points: %lf \n", num_steps);
+  printf(" points in: %lf \n", sum);
+  printf(" PI estimation: %lf \n", sum/num_steps * 4.0);
+
 }
